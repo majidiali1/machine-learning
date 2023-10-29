e56803f55591d216d78e99076315131ccab2e2b8
majidiali1 <en.majidi1373@gmail.com>
2023-10-29 15:55:51 +0330

    020807-regression 2 created

---
+++ regression2.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import numpy  as np
+import matplotlib.pyplot as plt
+import sklearn.metrics   as met
+
+from sklearn.model_selection import  train_test_split
+from sklearn.linear_model import  LinearRegression
+
+# Load data 
+X = np.array([10, 20 , 30 , 40 ,50 ,60  ,70  ,80  ,90  ,100])
+y = np.array([18, 41 , 61 , 79 ,70 ,120 ,141 ,150 ,120 ,200])
+
+X = X.reshape(-1, 1)
+y = y.reshape(-1, 1)
+
+df = pd.DataFrame(X)
+df ['y'] = y
+
+# split train and test data 
+xtrain,xtest,ytrain,ytest = train_test_split(X,y,test_size=0.3,random_state=42)
+
+# Plot data 
+plt.scatter(xtrain, ytrain ,  color='b')
+plt.scatter(xtest , ytest  ,  color='r')
+plt.xlabel('x')
+plt.ylabel('y')
+#plt.show()
+
+# regression model 
+model = LinearRegression()
+model.fit(xtrain , ytrain)
+
+# model parameters
+theta0 = model.intercept_ 
+theta1 = model.coef_
+
+# model prediction 
+ypred = model.predict(xtest)
+
+# model assesment 
+mse = met.mean_squared_error(ytest,ypred)
+
+# plot data 
+plt.scatter(xtrain, ytrain ,  color='b')
+plt.scatter(xtest , ytest  ,  color='r')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.plot(xtest,ypred,  color='black')
+
+plt.show()
+
+
+
+
+
+
+
+
+
