90aba1df9f04af504939535ddfbdba72b20892be
majidiali1 <en.majidi1373@gmail.com>
2023-10-30 11:25:59 +0330

    020808- regression dataset scatter plot

--- ml_functions.py
+++ ml_functions.py
@@ -1,5 +1,7 @@
-from sklearn import datasets
-import pandas as pd 
+from sklearn import datasets 
+import matplotlib.pyplot as plt
+
+import pandas as pd
 
 def load_dataset(to_df=True):
     d = datasets.load_diabetes()
@@ -12,3 +14,21 @@ def load_dataset(to_df=True):
         y = pd.Series(y)
 
     return x, y 
+
+def dataset_scatter_plots(xtrain, ytrain):
+    num_cols = len(xtrain.columns)
+
+    # Creating multiple figures with 2x2 subplots in each
+    for f in range((num_cols + 3) // 4):
+        fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(15, 10))
+        axes = axes.flatten()
+        for i in range(4 * f, min(4 * (f + 1), num_cols)):
+            ax = axes[i - 4 * f]
+            row = i % 4 // 2
+            col = i % 2
+            ax.scatter(xtrain[xtrain.columns[i]], ytrain, color='b', alpha=0.5)
+            #ax.set_title(f'{xtrain.columns[i]} vs ytrain')
+            ax.set_xlabel(xtrain.columns[i])
+            ax.set_ylabel('ytrain')
+        plt.tight_layout()
+        plt.show()
--- regression.py
+++ regression.py
@@ -1,6 +1,15 @@
-from sklearn.linear_model import LinearRegression
-import numpy as np 
+from sklearn.model_selection import  train_test_split
+import matplotlib.pyplot as plt
 
-import ml_functions as mfn
+import ml_functions as mlfn
 
-x,y = mfn.load_dataset(to_df=False)
+# Load dataset 
+x,y = mlfn.load_dataset(to_df=True)
+
+# data preprocessing ?! 
+
+# split train and test data 
+xtrain,xtest,ytrain,ytest = train_test_split(x,y,test_size=0.3,random_state=42)
+
+# plot dataset 
+mlfn.dataset_scatter_plots(xtrain, ytrain) 
