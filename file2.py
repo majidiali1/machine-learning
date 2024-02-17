83398f4c13f9adc9614c827dd38b77d58d0b65d4
majidiali1 <en.majidi1373@gmail.com>
2024-02-17 13:24:47 +0330

    021128-data preprocessing

--- file2.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd 
-
-## Load dataset 
-df = pd.read_csv('files/train_Titanic.csv')\
-
-# data information 
-dinfo = df.info()
-
--- general_functions.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd 
-
-def delete_df_columns(df, columns_list):
-    return df.drop(columns_list, axis=1)
-
-def groupby(df, columns_list, method='mean'):
-    return df.groupby(columns_list).mean()
-
